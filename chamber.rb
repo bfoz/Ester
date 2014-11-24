69307d4b3f58fc7ed81c9310815893b9ca49d541
Brandon Fosdick <bfoz@bfoz.net>
2014-11-24 00:03:48 -0800

    Fixed ChamberSidePanel slot generation
    The slots in the ChamberFrontPanel and not auto-generated from the tabs on ChamberSidePanel

--- chamber.rb
+++ chamber.rb
@@ -1,5 +1,51 @@
 require_relative 'PistonAssembly'
 
+extrusion :ChamberSidePanel do
+    attr_reader size: Size[PLATFORM_SIZE.y + 2*PISTON_WALL_THICKNESS, CHAMBER_BOX.z.cm + DeckPanel.thickness]
+
+    attr_reader repeat_count: 8
+    attr_reader slot_count: (repeat_count + 1)/2
+    attr_reader slot_length: (size.y - (1.cm + BottomPanel.thickness))/repeat_count
+
+    length PISTON_WALL_THICKNESS
+
+    tooth_depth = PISTON_WALL_THICKNESS
+
+    polygon do
+        start_at    [PISTON_WALL_THICKNESS,0]
+        up      1.cm + BottomPanel.thickness
+
+        i = 0
+        repeat to:[last.x, size.y], count:repeat_count do |step|
+            left   tooth_depth if i.even?
+            forward step
+            i += 1
+        end
+
+        up_to   size.y
+        up      DeckPanel.thickness
+        right   PLATFORM_SIZE.y
+        down    DeckPanel.thickness
+
+        i = 0
+        repeat to:[last.x, 1.cm + BottomPanel.thickness], count:repeat_count do |step|
+            left   tooth_depth if i.odd?
+            forward step
+            i += 1
+        end
+
+        down_to     0
+
+        repeat to:[tooth_depth, 0], count:2 do |step|
+            forward step/4
+            right   tooth_depth
+            forward step/2
+            left    tooth_depth
+            forward step/4
+        end
+    end
+end
+
 extrusion :ChamberFrontPanel do
     attr_reader rail_height: ACRYLIC_THICKNESS
     attr_reader size: Size[CHAMBER_BOX.x + 4.cm, CHAMBER_BOX.z]
@@ -26,8 +72,8 @@ extrusion :ChamberFrontPanel do
     end
 
     # Slots for the piston side walls
-    repeat center:[size.x/2, (size.y-rail_height)/2 - 3.375.mm], step:[2*PISTON_SIZE.x + 2*PISTON_WALL_THICKNESS, 4.55.cm], count:[2,4] do
-        rectangle center:[0,0], size:[length, 2.275.cm]
+    repeat center:[size.x/2, (size.y-rail_height)/2 - 3.375.mm], step:[2*PISTON_SIZE.x + 2*PISTON_WALL_THICKNESS, ChamberSidePanel.slot_length*2], count:[2,ChamberSidePanel.repeat_count/2] do
+        rectangle center:[0,0], size:[length, ChamberSidePanel.slot_length]
     end
 
     # Slots for the belts that connect the front and rear leadscrews
@@ -39,50 +85,6 @@ extrusion :ChamberFrontPanel do
     piston_slot_height = BUILD_VOLUME.z + PistonAssembly.flange_height
     repeat center:[size.x/2, size.y - PistonAssembly.height + PistonAssembly.flange_height - piston_slot_height/2], step:[BUILD_VOLUME.x + PLATFORM_SPACING, 0], count:[2,1] do
         rectangle center:[0,0], size:[PistonAssembly.bracket_width, piston_slot_height]
-    end
-end
-
-extrusion :ChamberSidePanel do
-    attr_reader size: Size[PLATFORM_SIZE.y + 2*ChamberFrontPanel.thickness, CHAMBER_BOX.z.cm + DeckPanel.thickness]
-
-    length PISTON_WALL_THICKNESS
-
-    tooth_depth = ChamberFrontPanel.length
-
-    polygon do
-        start_at    [PISTON_WALL_THICKNESS,0]
-        up      1.cm + ACRYLIC_THICKNESS
-        repeat to:[last.x, size.y], count:4 do |step|
-            left   tooth_depth
-            forward step/2
-            right   tooth_depth
-            forward step/2
-        end
-
-        up_to   size.y
-        # right   ACRYLIC_THICKNESS
-        up      ACRYLIC_THICKNESS
-        right   PLATFORM_SIZE.y
-        down    ACRYLIC_THICKNESS
-
-        # move_to size    # Top edge
-
-        repeat to:[last.x, 1.cm + ACRYLIC_THICKNESS], count:4 do |step|
-            forward step/2
-            left   tooth_depth
-            forward step/2
-            right   tooth_depth
-        end
-
-        down_to     0
-
-        repeat to:[tooth_depth, 0], count:2 do |step|
-            forward step/4
-            right   tooth_depth
-            forward step/2
-            left    tooth_depth
-            forward step/4
-        end
     end
 end
 
